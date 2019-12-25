d724c6a8b1e5b1e931c498ff990f21fc16cd3ebb
Robert Boyd III <rob@robertcboyd.com>
2019-12-25 18:32:34 -0500

    Update tests to follow proper Mocha style

--- tests/Tokenizer.test.ts
+++ tests/Tokenizer.test.ts
@@ -15,84 +15,13 @@ describe("tokenize()", () => {
             new Token(TokenType.IDENTIFER, "my_identifier", null, 1),
         ]);
     });
-});
 
-describe("isLegalIdentifierChar()", () => {
-    it("should check if a character is in [a-z0-9_]", () => {
-        expect(isLegalIdentifierChar("a")).to.equal(true);
-        expect(isLegalIdentifierChar("z")).to.equal(true);
-        expect(isLegalIdentifierChar("A")).to.equal(true);
-        expect(isLegalIdentifierChar("Z")).to.equal(true);
-        expect(isLegalIdentifierChar("0")).to.equal(true);
-        expect(isLegalIdentifierChar("9")).to.equal(true);
-        expect(isLegalIdentifierChar("_")).to.equal(true);
-        expect(isLegalIdentifierChar("-")).to.equal(false);
-        expect(isLegalIdentifierChar("\0")).to.equal(false);
-        expect(isLegalIdentifierChar("\"")).to.equal(false);
-        expect(isLegalIdentifierChar("'")).to.equal(false);
-        expect(isLegalIdentifierChar("\n")).to.equal(false);
-    });
-});
-
-describe("isAlphaNumeric()", () => {
-    it("should check if a character is in [a-z0-9]", () => {
-        expect(isAlphaNumeric("a")).to.equal(true);
-        expect(isAlphaNumeric("z")).to.equal(true);
-        expect(isAlphaNumeric("A")).to.equal(true);
-        expect(isAlphaNumeric("Z")).to.equal(true);
-        expect(isAlphaNumeric("0")).to.equal(true);
-        expect(isAlphaNumeric("9")).to.equal(true);
-        expect(isAlphaNumeric("_")).to.equal(false);
-        expect(isAlphaNumeric("-")).to.equal(false);
-        expect(isAlphaNumeric("\0")).to.equal(false);
-        expect(isAlphaNumeric("\"")).to.equal(false);
-        expect(isAlphaNumeric("'")).to.equal(false);
-        expect(isAlphaNumeric("\n")).to.equal(false);
-    });
-});
-
-describe("isAlpha()", () => {
-    it("should check if a character is [a-zA-Z]", () => {
-        expect(isAlpha("a")).to.equal(true);
-        expect(isAlpha("z")).to.equal(true);
-        expect(isAlpha("A")).to.equal(true);
-        expect(isAlpha("Z")).to.equal(true);
-        expect(isAlpha("0")).to.equal(false);
-        expect(isAlpha("9")).to.equal(false);
-        expect(isAlpha("_")).to.equal(false);
-        expect(isAlpha("-")).to.equal(false);
-        expect(isAlpha("\0")).to.equal(false);
-        expect(isAlpha("\"")).to.equal(false);
-        expect(isAlpha("'")).to.equal(false);
-        expect(isAlpha("\n")).to.equal(false);
-    });
-});
-
-describe("isNumber()", () => {
-    it("should check if a character is [0-9]", () => {
-        expect(isNumber("a")).to.equal(false);
-        expect(isNumber("z")).to.equal(false);
-        expect(isNumber("A")).to.equal(false);
-        expect(isNumber("Z")).to.equal(false);
-        expect(isNumber("0")).to.equal(true);
-        expect(isNumber("9")).to.equal(true);
-        expect(isNumber("_")).to.equal(false);
-        expect(isNumber("-")).to.equal(false);
-        expect(isNumber("\0")).to.equal(false);
-        expect(isNumber("\"")).to.equal(false);
-        expect(isNumber("'")).to.equal(false);
-    });
-});
-
-describe("tokenize() line number handling", () => {
     it("should provide correct line numbers", () => {
         let result = tokenize("hi\n7.543\n92");
         expect(result.map(t => t.line)).to.deep.equal([1, 1, 2, 2, 3]);
     });
-});
 
-describe("single line comment handling", () => {
-    it("should completely ignore everything after the comment", () => {
+    it("should ignore single line comments", () => {
         let result = tokenize(`let apple = 4.3 // apple = 0 *//**/??////
             let pancakes = apple * 3 // rando-stuff
         `);
@@ -111,18 +40,14 @@ describe("single line comment handling", () => {
             new Token(TokenType.STMT_TERM, "\n", null, 2),
         ])
     });
-});
 
-describe("multi line comment handling", () => {
-    it("should allow and ignore nested comments", () => {
+    it("should ignore nested multi-line comments", () => {
         let result = tokenize("/**//**\n let var = 32.432.32 *//* laskdjflaskdf 43*/apple");
         expect(result).to.deep.equal([
             new Token(TokenType.IDENTIFER, "apple", null, 1),
         ]);
     });
-});
 
-describe("tokenize() over multiple lines with variable declarations", () => {
     it("should use LET/VAR TokenType and STMT_TERM", () => {
         let result = tokenize(
             `let a = 23
@@ -139,5 +64,93 @@ describe("tokenize() over multiple lines with variable declarations", () => {
             new Token(TokenType.EQUAL, "=", null, 2),
             new Token(TokenType.NUMBER, "46", 46, 2),
         ]);
+    });
+});
+
+describe("isLegalIdentifierChar()", () => {
+    it("should return true if a character is [a-zA-Z]", () => {
+        expect(isLegalIdentifierChar("a")).to.equal(true);
+        expect(isLegalIdentifierChar("z")).to.equal(true);
+        expect(isLegalIdentifierChar("A")).to.equal(true);
+        expect(isLegalIdentifierChar("Z")).to.equal(true);
+    });
+
+    it("should return true if a character is [0-9]", () => {
+        expect(isLegalIdentifierChar("0")).to.equal(true);
+        expect(isLegalIdentifierChar("9")).to.equal(true);
+        expect(isLegalIdentifierChar("-")).to.equal(false);
+    });
+
+    it("should return true if a character is [_]", () => {
+        expect(isLegalIdentifierChar("_")).to.equal(true);
+    });
+
+    it("should return false if a character is [-\0\"\'\n]", () => {
+       expect(isLegalIdentifierChar("\0")).to.equal(false);
+       expect(isLegalIdentifierChar("\"")).to.equal(false);
+       expect(isLegalIdentifierChar("'")).to.equal(false);
+       expect(isLegalIdentifierChar("\n")).to.equal(false);
+    });
+});
+
+describe("isAlphaNumeric()", () => {
+    it("should return true if a character is [a-zA-Z]", () => {
+        expect(isAlphaNumeric("a")).to.equal(true);
+        expect(isAlphaNumeric("z")).to.equal(true);
+        expect(isAlphaNumeric("A")).to.equal(true);
+        expect(isAlphaNumeric("Z")).to.equal(true);
+    });
+
+    it("should return true if a character is [0-9]", () => {
+        expect(isAlphaNumeric("0")).to.equal(true);
+        expect(isAlphaNumeric("9")).to.equal(true);
+    });
+
+    it("should return false if a character is [_-\0\"\'\n]", () => {
+        expect(isAlphaNumeric("_")).to.equal(false);
+        expect(isAlphaNumeric("-")).to.equal(false);
+        expect(isAlphaNumeric("\0")).to.equal(false);
+        expect(isAlphaNumeric("\"")).to.equal(false);
+        expect(isAlphaNumeric("'")).to.equal(false);
+        expect(isAlphaNumeric("\n")).to.equal(false);
+    });
+});
+
+describe("isAlpha()", () => {
+    it("should return true if a character is [a-zA-Z]", () => {
+        expect(isAlpha("a")).to.equal(true);
+        expect(isAlpha("z")).to.equal(true);
+        expect(isAlpha("A")).to.equal(true);
+        expect(isAlpha("Z")).to.equal(true);
+    });
+
+    it("should return false if a character is [_-\0\"\'\n]", () => {
+        expect(isAlpha("0")).to.equal(false);
+        expect(isAlpha("9")).to.equal(false);
+        expect(isAlpha("_")).to.equal(false);
+        expect(isAlpha("-")).to.equal(false);
+        expect(isAlpha("\0")).to.equal(false);
+        expect(isAlpha("\"")).to.equal(false);
+        expect(isAlpha("'")).to.equal(false);
+        expect(isAlpha("\n")).to.equal(false);
+    });
+});
+
+describe("isNumber()", () => {
+    it("should return true if a character is [0-9]", () => {
+        expect(isNumber("0")).to.equal(true);
+        expect(isNumber("9")).to.equal(true);
+    });
+
+    it("should return false if a character is not [0-9]", () => {
+        expect(isNumber("a")).to.equal(false);
+        expect(isNumber("z")).to.equal(false);
+        expect(isNumber("A")).to.equal(false);
+        expect(isNumber("Z")).to.equal(false);
+        expect(isNumber("_")).to.equal(false);
+        expect(isNumber("-")).to.equal(false);
+        expect(isNumber("\0")).to.equal(false);
+        expect(isNumber("\"")).to.equal(false);
+        expect(isNumber("'")).to.equal(false);
     });
 });
