2e02478be09adf72b19880e510093feb5a0b5bc1
Robert Boyd III <rob@robertcboyd.com>
2019-12-27 16:04:31 -0500

    Add test for parse()

--- test/Parser.test.ts
+++ test/Parser.test.ts
@@ -2,9 +2,9 @@ import tokenize from "../src/Tokenizer";
 import parse from "../src/Parser";
 import { expect } from "chai";
 
-/*
 describe("parse()", () => {
-    it("should throw if ", () => {
+    it("should throw if a statement isn't terminated by a newline", () => {
+        let src = "let 2 = 34 let 3 = 14"
+        expect(() => parse(tokenize(src))).to.throw("Expected a newline!");
     });
 });
-*/
