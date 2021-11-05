cc8147532517b2ca546c6f3518c46008d5160604
Robert Boyd III <rob@robertcboyd.com>
2021-11-05 13:59:00 -0400

    Improve array printing

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,6 +1,6 @@
 import { UnboxedValue } from "./Value";
 
-let printfn = (val: UnboxedValue) => console.log(val.toString());
+let printfn = (val: UnboxedValue) => console.log(toString(val));
 
 export function setPrintFn(fn: (val: UnboxedValue) => void) {
     printfn = fn;
@@ -8,4 +8,12 @@ export function setPrintFn(fn: (val: UnboxedValue) => void) {
 
 export function stlPrint(val: UnboxedValue) {
     printfn(val);
+}
+
+function toString(val: UnboxedValue): string {
+    if (Array.isArray(val)) {
+        return `[${val.map(boxed => toString(boxed.value))}]`;
+    } else {
+        return val.toString();
+    }
 }
