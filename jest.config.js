22204cc8c826e0e6a713d28f22528bf5fb0a6f74
Robert Boyd III <git@robertboyd.dev>
2022-02-19 03:26:50 -0500

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
-// Sync object
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest",
+  },
+  extensionsToTreatAsEsm: [".ts"],
 	testPathIgnorePatterns: ["<rootDir>/dist"],
-};
+}
 
-module.exports = config;
+export default config;
