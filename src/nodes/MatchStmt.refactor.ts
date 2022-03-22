3bacf4c80e635e5e0bf509863b4990c26204f182
Robert Boyd III <git@robertboyd.dev>
2022-03-21 22:38:55 -0400

    Refactor MatchStmt and helpers to functional style

---
+++ src/nodes/MatchStmt.refactor.ts
@@ -0,0 +1,71 @@
+import { RuntimePanic } from "../Debug.js";
+import { Expr } from "../Expr.refactor.js";
+import { equal } from "../Interpreter.js";
+import Scope from "../Scope.js";
+import Token from "../Token.js";
+import { Value } from "../Value.js";
+import PrimaryExpr from "./PrimaryExpr.js";
+
+export type MatchStmt = Expr & {
+	expr: Expr;
+	cases: MatchCase[];
+};
+
+export const MatchStmt = (
+	expr: Expr,
+	cases: MatchCase[],
+	tokens: Token[]
+): MatchStmt => {
+	return {
+		expr,
+		cases,
+		tokens,
+		eval(scope: Scope) {
+			const rootExpr = this.expr;
+			let [matchExprValue, newScope] = rootExpr.eval(scope);
+			if (matchExprValue === null) {
+				throw RuntimePanic("MatchExpr cannot be nothing!");
+			}
+			for (const matchCase of this.cases) {
+				if (matchCase.matchExpr instanceof UnderscoreExpr) {
+					return matchCase.expr.eval(newScope);
+				}
+				// FIXME decide if side effects are legal in a match expression
+				const [caseValue, newScope2] = matchCase.matchExpr.eval(newScope);
+				newScope = newScope2;
+				// FIXME
+				if (equal(caseValue!, matchExprValue).value) {
+					return matchCase.expr.eval(newScope);
+				}
+			}
+			throw RuntimePanic("Pattern match failed.");
+		},
+	};
+};
+
+export type MatchCase = {
+	matchExpr: UnderscoreExpr | PrimaryExpr;
+	expr: Expr;
+};
+
+export const MatchCase = (
+	matchExpr: UnderscoreExpr | PrimaryExpr,
+	expr: Expr
+): MatchCase => {
+	return {
+		matchExpr,
+		expr,
+	};
+};
+
+export type UnderscoreExpr = Expr;
+
+export const UnderscoreExpr = (tokens: Token[]): UnderscoreExpr => {
+	return {
+		tokens,
+		eval(scope: Scope): [Value, Scope] {
+			// FIXME
+			throw RuntimePanic("Tried to evaluate an UnderscoreExpr");
+		},
+	};
+};
