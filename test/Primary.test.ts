9f812536ab8257ae63df2d7ea9f4704740eb2e18
Robert Boyd III <git@robertboyd.dev>
2022-03-21 21:27:52 -0400

    Create assertEqual() helper for ast tests

--- test/Primary.test.ts
+++ test/Primary.test.ts
@@ -3,17 +3,21 @@ import { it, expect } from "vitest";
 import { Node, print, x } from "code-red";
 import StlNumber from "../src/StlNumber.js";
 
+const assertEqual = (node1: Node, node2: Node) => {
+	expect(print(node1).code).toEqual(print(node2).code);
+}
+
 it("should compile booleans", () => {
 	const node: Node = new PrimaryExpr(true, []).estree();
-	expect(print(node).code).toEqual(print(x`{stlValue: true}`).code);
+	assertEqual(node, x`{stlValue: true}`);
 });
 
 it("should compile strings", () => {
 	const node: Node = new PrimaryExpr("hello", []).estree();
-	expect(print(node).code).toEqual(print(x`{stlValue: "hello"}`).code);
+	assertEqual(node, x`{stlValue: "hello"}`);
 });
 
 it("should compile numbers", () => {
 	const node: Node = new PrimaryExpr(StlNumber.of(5), []).estree();
-	expect(print(node).code).toEqual(print(x`{stlValue: {top: 5n, bottom: 1n}}`).code);
+	assertEqual(node, x`{stlValue: {top: 5n, bottom: 1n}}`);
 });
