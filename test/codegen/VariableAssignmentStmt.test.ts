d4454b48c48dc3f7987fe1af2eacbe08a60aa440
Robert Boyd III <git@robertboyd.dev>
2022-03-23 17:14:58 -0400

    Test VariableAssignmentStmt codegen

---
+++ test/codegen/VariableAssignmentStmt.test.ts
@@ -0,0 +1,27 @@
+import { x } from "code-red";
+import { describe, it } from "vitest";
+import { BinaryExpr } from "../../src/nodes/BinaryExpr";
+import { PrimaryExpr } from "../../src/nodes/PrimaryExpr";
+import { VariableAssignmentStmt } from "../../src/nodes/VariableAssignmentStmt";
+import { VariableExpr } from "../../src/nodes/VariableExpr";
+import StlNumber from "../../src/StlNumber";
+import Token from "../../src/Token";
+import TokenType from "../../src/TokenType";
+import { assertEqual } from "../Helpers";
+
+describe("VariableExpr codegen", () => {
+	it("should compile an assignment with a BinaryExpr", () => {
+		const node = VariableAssignmentStmt(
+			VariableExpr("a"),
+			BinaryExpr(
+				PrimaryExpr(StlNumber.of(2)),
+				{ type: TokenType.PLUS } as Token,
+				PrimaryExpr(StlNumber.of(2))
+			)
+		).estree();
+		assertEqual(
+			node,
+			x`a = stlAdd({stlValue: {top: 2n, bottom: 1n}}, {stlValue: {top: 2n, bottom: 1n}})`
+		);
+	});
+});
