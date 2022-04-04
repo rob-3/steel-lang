0f07bb84addac0efc8190f82df684c66a83415d9
Robert Boyd III <git@robertboyd.dev>
2022-04-04 18:20:48 -0400

    Add tests for more arguments

--- test/codegen/CallExpr.test.ts
+++ test/codegen/CallExpr.test.ts
@@ -1,12 +1,32 @@
 import { Node, x } from "code-red";
 import { describe, it } from "vitest";
 import { CallExpr } from "../../src/nodes/CallExpr";
+import { PrimaryExpr } from "../../src/nodes/PrimaryExpr";
 import { VariableExpr } from "../../src/nodes/VariableExpr";
+import StlNumber from "../../src/StlNumber";
 import { assertEqual } from "../Helpers";
 
 describe("CallExpr codegen", () => {
 	it("should compile a no argument call", () => {
 		const node: Node = CallExpr(VariableExpr("hello"), []).estree();
 		assertEqual(node, x`hello()`);
+	});
+
+	it("should compile a one argument call", () => {
+		const node: Node = CallExpr(VariableExpr("hello"), [
+			PrimaryExpr(StlNumber.of(2)),
+		]).estree();
+		assertEqual(node, x`hello({stlValue: {top: 2n, bottom: 1n}})`);
+	});
+
+	it("should compile a two argument call", () => {
+		const node: Node = CallExpr(VariableExpr("hello"), [
+			PrimaryExpr(StlNumber.of(2)),
+			PrimaryExpr("foo"),
+		]).estree();
+		assertEqual(
+			node,
+			x`hello({stlValue: {top: 2n, bottom: 1n}}, {stlValue: "foo"})`
+		);
 	});
 });
