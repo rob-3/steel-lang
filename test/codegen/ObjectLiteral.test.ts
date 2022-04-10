1ed12827963d5a2778a5487130130b8a4e2caf13
Robert Boyd III <git@robertboyd.dev>
2022-04-10 00:30:38 -0400

    Test ObjectLiteral codegen

---
+++ test/codegen/ObjectLiteral.test.ts
@@ -0,0 +1,33 @@
+import { Node, x } from "code-red";
+import { describe, it } from "vitest";
+import { ObjectLiteral } from "../../src/nodes/ObjectLiteral";
+import { PrimaryExpr } from "../../src/nodes/PrimaryExpr";
+import StlNumber from "../../src/StlNumber";
+import { assertEqual } from "../Helpers";
+
+describe("ObjectLiteral codegen", () => {
+	it("should compile an empty object literal", () => {
+		const node: Node = ObjectLiteral(new Map()).estree();
+		assertEqual(node, x`{stlValue: {}}`);
+	});
+
+	it("should compile a non-empty object literal", () => {
+		const node: Node = ObjectLiteral(
+			new Map([
+				["sky", PrimaryExpr("blue")],
+				["grass", PrimaryExpr("green")],
+				["pi", PrimaryExpr(StlNumber.of("3.14"))],
+			])
+		).estree();
+		assertEqual(
+			node,
+			x`{
+				stlValue: {
+					sky: {stlValue: "blue"},
+					grass: {stlValue: "green"},
+					pi: {stlValue: {top: 157n, bottom: 50n}}
+				}
+			}`
+		);
+	});
+});
