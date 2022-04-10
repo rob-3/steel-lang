284b8edd4f4e5667ba3dc759c4727c6c339dd95e
Robert Boyd III <git@robertboyd.dev>
2022-04-10 00:30:09 -0400

    Implement ObjectLiteral codegen

--- src/nodes/ObjectLiteral.ts
+++ src/nodes/ObjectLiteral.ts
@@ -4,10 +4,12 @@ import Scope from "../Scope.js";
 import { Value, Box } from "../Value.js";
 import StlObject from "../StlObject.js";
 import { RuntimePanic } from "../Debug.js";
+import { Node, x } from "code-red";
 
 export type ObjectLiteral = ExprBase & {
 	type: "ObjectLiteral";
 	properties: Map<string, Expr>;
+	estree(): Node;
 };
 
 export const ObjectLiteral = (
@@ -31,6 +33,19 @@ export const ObjectLiteral = (
 				map.set(key, result);
 			}
 			return [new Box(new StlObject(map)), currentScope];
+		},
+		estree() {
+			const pairs = Array.from(this.properties.entries());
+			if (pairs.length === 0) {
+				return x`{stlValue: {}}`;
+			}
+			const colonsAndCommas = Array(pairs.length).fill([":", ","]).flat();
+			colonsAndCommas.pop();
+			const strings = ["{stlValue: {", ...colonsAndCommas, "}}"];
+			return x(
+				strings as unknown as TemplateStringsArray,
+				...pairs.flatMap(([key, value]) => [key, value.estree()])
+			);
 		},
 	};
 };
