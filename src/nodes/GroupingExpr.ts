7e5c00f314b694d5cd60a84784e551db1fe032df
Robert Boyd III <git@robertboyd.dev>
2022-04-11 17:15:08 -0400

    Implement initial GroupingExpr codegen

--- src/nodes/GroupingExpr.ts
+++ src/nodes/GroupingExpr.ts
@@ -1,3 +1,4 @@
+import { Node } from "code-red";
 import { Expr, ExprBase } from "../Expr.js";
 import Scope from "../Scope.js";
 import Token from "../Token.js";
@@ -5,15 +6,22 @@ import Token from "../Token.js";
 export type GroupingExpr = ExprBase & {
 	type: "GroupingExpr";
 	expr: Expr;
+	estree(): Node;
 };
 
-export const GroupingExpr = (expr: Expr, tokens: Token[] = []): GroupingExpr => {
+export const GroupingExpr = (
+	expr: Expr,
+	tokens: Token[] = []
+): GroupingExpr => {
 	return {
 		type: "GroupingExpr",
 		expr,
 		tokens,
 		eval(scope: Scope) {
 			return this.expr.eval(scope);
+		},
+		estree() {
+			return this.expr.estree();
 		},
 	};
 };
