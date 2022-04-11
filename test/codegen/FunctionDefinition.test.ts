ed60e50fb2b9a64d3cfd2a22b9f6dfa605d4d69b
Robert Boyd III <git@robertboyd.dev>
2022-04-11 16:58:55 -0400

    Test FunctionDefinition codegen

---
+++ test/codegen/FunctionDefinition.test.ts
@@ -0,0 +1,25 @@
+import { Node, x } from "code-red";
+import { describe, it } from "vitest";
+import { BinaryExpr } from "../../src/nodes/BinaryExpr";
+import { FunctionDefinition } from "../../src/nodes/FunctionDefinition";
+import { FunctionExpr } from "../../src/nodes/FunctionExpr";
+import { VariableDeclarationStmt } from "../../src/nodes/VariableDeclarationStmt";
+import { VariableExpr } from "../../src/nodes/VariableExpr";
+import TokenType from "../../src/TokenType";
+import { assertEqual } from "../Helpers";
+
+describe("FunctionDefinition codegen", () => {
+	it("should compile function definitions", () => {
+		const node: Node = FunctionDefinition(
+			VariableDeclarationStmt(
+				"sum",
+				true,
+				FunctionExpr(
+					["a", "b"],
+					BinaryExpr(VariableExpr("a"), TokenType.PLUS, VariableExpr("b"))
+				)
+			)
+		).estree();
+		assertEqual(node, x`sum = (a, b) => stlAdd(a, b)`);
+	});
+});
