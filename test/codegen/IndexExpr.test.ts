55f312ecaf022ef2665cabb393b2859f3dfc14c9
Robert Boyd III <git@robertboyd.dev>
2022-04-11 17:40:26 -0400

    Test IndexExpr codegen

---
+++ test/codegen/IndexExpr.test.ts
@@ -0,0 +1,19 @@
+import { Node, x } from "code-red";
+import { describe, it } from "vitest";
+import { IndexExpr } from "../../src/nodes/IndexExpr";
+import { PrimaryExpr } from "../../src/nodes/PrimaryExpr";
+import StlNumber from "../../src/StlNumber";
+import { assertEqual } from "../Helpers";
+
+describe("IndexExpr codegen", () => {
+	it("should compile a basic literal index", () => {
+		const node: Node = IndexExpr(
+			"array",
+			PrimaryExpr(StlNumber.of(2))
+		).estree();
+		assertEqual(
+			node,
+			x`stlUnwrap(array)[stlUnwrap({stlValue: {top: 2n, bottom: 1n}})]`
+		);
+	});
+});
