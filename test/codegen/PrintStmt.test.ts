511fb0aada97d1be51d1945255ea01f26f920eac
Robert Boyd III <git@robertboyd.dev>
2022-04-23 00:31:12 -0400

    Test PrintStmt codegen

---
+++ test/codegen/PrintStmt.test.ts
@@ -0,0 +1,13 @@
+import { x } from "code-red";
+import { describe, it } from "vitest";
+import { PrimaryExpr } from "../../src/nodes/PrimaryExpr";
+import { PrintStmt } from "../../src/nodes/PrintStmt";
+import StlNumber from "../../src/StlNumber";
+import { assertEqual } from "../Helpers";
+
+describe("PrintStmt codegen", () => {
+	it("should compile a print statement", () => {
+		const node = PrintStmt(PrimaryExpr(StlNumber.of(5))).estree();
+		assertEqual(node, x`stlPrint({stlValue: {top: 5n, bottom: 1n}})`);
+	});
+});
