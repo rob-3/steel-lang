ca28bd52ef474f1d8eb8e69d249cc4304c302ee7
Robert Boyd III <git@robertboyd.dev>
2022-05-07 17:02:32 -0400

    Implement ReturnStmt codegen

--- src/nodes/ReturnStmt.ts
+++ src/nodes/ReturnStmt.ts
@@ -1,3 +1,4 @@
+import { b, Node } from "code-red";
 import { Expr, ExprBase } from "../Expr.js";
 import Scope from "../Scope.js";
 import Token from "../Token.js";
@@ -5,6 +6,7 @@ import Token from "../Token.js";
 export type ReturnStmt = ExprBase & {
 	type: "ReturnStmt";
 	value: Expr;
+	estree(): Node;
 };
 
 export const ReturnStmt = (value: Expr, tokens: Token[] = []): ReturnStmt => {
@@ -14,6 +16,9 @@ export const ReturnStmt = (value: Expr, tokens: Token[] = []): ReturnStmt => {
 		tokens,
 		eval(scope: Scope) {
 			return this.value.eval(scope);
+		},
+		estree(): Node {
+			return b`return ${this.value.estree()}`[0];
 		},
 	};
 };
