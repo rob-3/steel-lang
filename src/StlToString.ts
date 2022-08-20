1cf45392747134e44a6bc972d6f8f2b6927c66ff
Robert Boyd III <git@robertboyd.dev>
2022-08-20 16:06:27 -0400

    Stringify StlObjects correctly

--- src/StlToString.ts
+++ src/StlToString.ts
@@ -1,4 +1,5 @@
 import { StlFunction } from "./StlFunction.js";
+import StlObject from "./StlObject.js";
 import { UnboxedValue } from "./Value.js";
 
 export function stlToString(val: UnboxedValue): string {
@@ -6,6 +7,10 @@ export function stlToString(val: UnboxedValue): string {
 		return `[${val.map((boxed) => stlToString(boxed.value)).join(", ")}]`;
 	} else if (val instanceof StlFunction) {
 		return "<function>";
+	} else if (val instanceof StlObject) {
+		return `{ ${[...val.properties.entries()]
+			.map(([key, value]) => `${key}: ${stlToString(value.value)}`)
+			.join(", ")} }`;
 	} else {
 		return val.toString();
 	}
