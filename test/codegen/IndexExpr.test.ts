b43467b319fb7d663e419cb0d72c887b9a8a3940
Robert Boyd III <git@robertboyd.dev>
2022-08-20 16:30:13 -0400

    Test indexing with a variable

--- test/codegen/IndexExpr.test.ts
+++ test/codegen/IndexExpr.test.ts
@@ -2,6 +2,7 @@ import { x } from "code-red";
 import { describe, it } from "vitest";
 import { IndexExpr } from "../../src/nodes/IndexExpr";
 import { PrimaryExpr } from "../../src/nodes/PrimaryExpr";
+import { VariableExpr } from "../../src/nodes/VariableExpr";
 import StlNumber from "../../src/StlNumber";
 import { assertEqual } from "../Helpers";
 
@@ -12,5 +13,10 @@ describe("IndexExpr codegen", () => {
 			node,
 			x`stlUnwrap(array)[stlUnwrap({stlValue: {top: 2n, bottom: 1n}})]`
 		);
+	});
+
+	it("should compile a variable index", () => {
+		const node = IndexExpr("array", VariableExpr("a")).estree();
+		assertEqual(node, x`stlUnwrap(array)[stlUnwrap(a)]`);
 	});
 });
