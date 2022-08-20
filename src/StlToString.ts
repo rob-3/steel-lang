126aa954297414e6e13d751580eada438725bac0
Robert Boyd III <git@robertboyd.dev>
2022-08-20 15:48:48 -0400

    Print arrays with a space

--- src/StlToString.ts
+++ src/StlToString.ts
@@ -3,7 +3,7 @@ import { UnboxedValue } from "./Value.js";
 
 export function stlToString(val: UnboxedValue): string {
 	if (Array.isArray(val)) {
-		return `[${val.map((boxed) => stlToString(boxed.value))}]`;
+		return `[${val.map((boxed) => stlToString(boxed.value)).join(", ")}]`;
 	} else if (val instanceof StlFunction) {
 		return "<function>";
 	} else {
