ae7584f47788d1883120742f4e4d0f868172a6fd
Robert Boyd III <git@robertboyd.dev>
2022-08-20 15:17:50 -0400

    Print functions properly

--- src/StlToString.ts
+++ src/StlToString.ts
@@ -1,8 +1,11 @@
+import { StlFunction } from "./StlFunction.js";
 import { UnboxedValue } from "./Value.js";
 
 export function stlToString(val: UnboxedValue): string {
 	if (Array.isArray(val)) {
 		return `[${val.map((boxed) => stlToString(boxed.value))}]`;
+	} else if (val instanceof StlFunction) {
+		return "<function>";
 	} else {
 		return val.toString();
 	}
