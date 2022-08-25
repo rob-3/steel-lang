b868778a5ad72d930b5224d6b8b4bd467b2b0965
Robert Boyd III <git@robertboyd.dev>
2022-08-25 16:50:07 -0400

    Test lambda with inner variable declaration

--- test/codegen/FunctionExpr.test.ts
+++ test/codegen/FunctionExpr.test.ts
@@ -1,8 +1,12 @@
 import { x } from "code-red";
 import { describe, it } from "vitest";
 import { BinaryExpr } from "../../src/nodes/BinaryExpr";
+import { BlockStmt } from "../../src/nodes/BlockStmt";
 import { FunctionExpr } from "../../src/nodes/FunctionExpr";
+import { PrimaryExpr } from "../../src/nodes/PrimaryExpr";
+import { VariableDeclarationStmt } from "../../src/nodes/VariableDeclarationStmt";
 import { VariableExpr } from "../../src/nodes/VariableExpr";
+import StlNumber from "../../src/StlNumber";
 import TokenType from "../../src/TokenType";
 import { assertEqual } from "../Helpers";
 
@@ -16,5 +20,26 @@ describe("FunctionExpr codegen", () => {
 			BinaryExpr(VariableExpr("a"), TokenType.PLUS, VariableExpr("b"))
 		).estree();
 		assertEqual(node, x`(a, b) => stlAdd(a, b)`);
+	});
+
+	it("should compile a lambda with an inner variable declaration", () => {
+		const node = FunctionExpr(
+			[
+				{ name: "a", isImmutable: true },
+				{ name: "b", isImmutable: true },
+			],
+			BlockStmt([
+				VariableDeclarationStmt("c", true, PrimaryExpr(StlNumber.of(5))),
+				BinaryExpr(VariableExpr("a"), TokenType.PLUS, VariableExpr("b")),
+			])
+		).estree();
+		assertEqual(
+			node,
+			x`(a, b) => (() => {
+			let c;
+			c = { stlValue: { top: 5n, bottom: 1n }};
+			return stlAdd(a, b)
+		})()`
+		);
 	});
 });
