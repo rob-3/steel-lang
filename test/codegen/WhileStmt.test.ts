d0c4e31d697a46babc89310bd93e704e7d6f3543
Robert Boyd III <git@robertboyd.dev>
2022-08-25 16:40:39 -0400

    Test while statement with internal declaration

--- test/codegen/WhileStmt.test.ts
+++ test/codegen/WhileStmt.test.ts
@@ -3,6 +3,7 @@ import { describe, it } from "vitest";
 import { BinaryExpr } from "../../src/nodes/BinaryExpr";
 import { PrimaryExpr } from "../../src/nodes/PrimaryExpr";
 import { VariableAssignmentStmt } from "../../src/nodes/VariableAssignmentStmt";
+import { VariableDeclarationStmt } from "../../src/nodes/VariableDeclarationStmt";
 import { VariableExpr } from "../../src/nodes/VariableExpr";
 import { WhileStmt } from "../../src/nodes/WhileStmt";
 import StlNumber from "../../src/StlNumber";
@@ -28,6 +29,28 @@ describe("WhileStmt codegen", () => {
 			}
 			return ret;
 		})()`
+		);
+	});
+
+	it("should compile a while statement with an internal variable declaration", () => {
+		const node = WhileStmt(
+			BinaryExpr(
+				VariableExpr("a"),
+				TokenType.EQUAL_EQUAL,
+				PrimaryExpr(StlNumber.of(2))
+			),
+			VariableDeclarationStmt("hello", true, PrimaryExpr(StlNumber.of(5)))
+		).estree();
+		assertEqual(
+			node,
+			x`(() => {
+			let ret;
+			while (stlEqual(a, {stlValue: {top: 2n, bottom: 1n}})) {
+				let hello;
+				ret = (hello = { stlValue: { top: 5n, bottom: 1n } });
+			}
+			return ret;
+			})()`
 		);
 	});
 });
