615e3c9a2d80d8e6884b8b6471e17e6a2f92843f
Robert Boyd III <git@robertboyd.dev>
2022-09-06 17:04:32 -0400

    Test variable declarations in if statements

--- test/codegen/IfStmt.test.ts
+++ test/codegen/IfStmt.test.ts
@@ -2,6 +2,7 @@ import { x } from "code-red";
 import { describe, it } from "vitest";
 import { IfStmt } from "../../src/nodes/IfStmt";
 import { PrimaryExpr } from "../../src/nodes/PrimaryExpr";
+import { VariableDeclarationStmt } from "../../src/nodes/VariableDeclarationStmt";
 import StlNumber from "../../src/StlNumber";
 import { assertEqual } from "../Helpers";
 
@@ -15,6 +16,38 @@ describe("IfStmt codegen", () => {
 		assertEqual(
 			node,
 			x`stlUnwrap({stlValue: true}) ? {stlValue: {top: 2n, bottom: 1n}} : {stlValue: {top: 3n, bottom: 1n}}`
+		);
+	});
+
+	it("should compile an if statement with internal variable declaration", () => {
+		const node = IfStmt(
+			PrimaryExpr(true),
+			VariableDeclarationStmt("greeting", true, PrimaryExpr("hello, world")),
+			PrimaryExpr(StlNumber.of(3))
+		).estree();
+		assertEqual(
+			node,
+			x`stlUnwrap({stlValue: true}) ? (() => {
+				let greeting;
+				return greeting = { stlValue: "hello, world" }
+			})() : {stlValue: {top: 3n, bottom: 1n}}`
+		);
+	});
+
+	it("should compile an else statement with internal variable declaration", () => {
+		const node = IfStmt(
+			PrimaryExpr(true),
+			PrimaryExpr(StlNumber.of(3)),
+			VariableDeclarationStmt("greeting", true, PrimaryExpr("hello, world"))
+		).estree();
+		assertEqual(
+			node,
+			x`stlUnwrap({stlValue: true}) ?
+				{stlValue: {top: 3n, bottom: 1n}} :
+				(() => {
+					let greeting;
+					return greeting = { stlValue: "hello, world" }
+				})()`
 		);
 	});
 });
