74dcc62f9c268ac79e98d18faa0da5ebe2937228
Robert Boyd III <git@robertboyd.dev>
2022-09-06 17:20:35 -0400

    Extract standard library to new file

---
+++ src/StandardLibrary.ts
@@ -0,0 +1,31 @@
+export const standardLibrary =
+`const stlEqual = (a, b) =>
+	a.stlValue.top === b.stlValue.top && a.stlValue.bottom === b.stlValue.bottom;
+const stlAdd = (a, b) => ({
+	stlValue: {
+		top: a.stlValue.top + b.stlValue.top,
+		bottom: a.stlValue.bottom,
+	},
+});
+const stlSubtract = (a, b) => ({
+	stlValue: {
+		top: a.stlValue.top - b.stlValue.top,
+		bottom: a.stlValue.bottom,
+	},
+});
+const print = (arg) => {
+  const { stlValue } = arg;
+  if (typeof arg === "function") {
+    console.log("<lambda>");
+  } else if (typeof stlValue === "string" || typeof stlValue === "boolean") {
+    console.log(stlValue);
+  } else if ('top' in stlValue) {
+    if (stlValue.bottom === 1n) {
+      console.log(stlValue.top.toString());
+    } else {
+      console.log(\`\${stlValue.top}/\${stlValue.bottom}\`);
+    }
+  } else {
+    console.log(stlValue);
+  }
+};`;
--- src/steel.ts
+++ src/steel.ts
@@ -4,6 +4,7 @@ import { Expr, IdentifierDeclaration } from "./Expr.js";
 import { exprEval } from "./Interpreter.js";
 import parse from "./Parser.js";
 import Scope from "./Scope.js";
+import { standardLibrary } from "./StandardLibrary.js";
 import { StlFunction } from "./StlFunction.js";
 import tokenize from "./Tokenizer.js";
 import { UnboxedValue } from "./Value.js";
@@ -111,36 +112,7 @@ export function compile(src: string, filename: string) {
 				.map(({ identifier }) => `let ${identifier};`)
 				.join("\n");
 			const outputJS = exprStrings.join(";\n");
-			return `const stlEqual = (a, b) =>
-	a.stlValue.top === b.stlValue.top && a.stlValue.bottom === b.stlValue.bottom;
-const stlAdd = (a, b) => ({
-	stlValue: {
-		top: a.stlValue.top + b.stlValue.top,
-		bottom: a.stlValue.bottom,
-	},
-});
-const stlSubtract = (a, b) => ({
-	stlValue: {
-		top: a.stlValue.top - b.stlValue.top,
-		bottom: a.stlValue.bottom,
-	},
-});
-const print = (arg) => {
-  const { stlValue } = arg;
-  if (typeof arg === "function") {
-    console.log("<lambda>");
-  } else if (typeof stlValue === "string" || typeof stlValue === "boolean") {
-    console.log(stlValue);
-  } else if ('top' in stlValue) {
-    if (stlValue.bottom === 1n) {
-      console.log(stlValue.top.toString());
-    } else {
-      console.log(\`\${stlValue.top}/\${stlValue.bottom}\`);
-    }
-  } else {
-    console.log(stlValue);
-  }
-};
+			return `${standardLibrary}
 ${letDecls}
 ${outputJS}`;
 		},
